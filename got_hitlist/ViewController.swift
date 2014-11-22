b4b2cd5e2e9b9c0f395f74d625c699ce593b0fab
Kevin Lau <thekevlau@gmail.com>
2014-11-21 23:11:35 -0500

    Initial Commit

---
+++ got_hitlist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  got_hitlist
+//
+//  Created by Kevin on 2014-11-21.
+//  Copyright (c) 2014 the.baratheon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
